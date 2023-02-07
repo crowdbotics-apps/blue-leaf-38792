b635ee717fcabcf4ca1cd8fdd09142d89b7b2892
Crowdbotics <team@crowdbotics.com>
2023-02-07 17:25:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-leaf-38792",
+  url: "https://blue-leaf-38792.botics.co",
+  api: "https://blue-leaf-38792.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
